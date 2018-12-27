7896d3d30f78e4acf145b0d3931e2a995aae8641
IvanPinna <ivanpinaarevalo@gmail.com>
2018-12-27 14:08:25 +0100

    algoritmo incompleto fusion

---
+++ ordenacionfusion.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+/*int**/ ordenarporfusion(int* vector, int tam){
+    
+    if(tam < 3)
+    {
+        insercion(vector); //Ordenamos por insercion.
+    }
+    else
+    {
+        //Dividir el array en dos subvectores. 
+    }
+    //return ;
+}
+
+int main()
+{
+    int p[13] = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9};
+    fusion(p, 13); 
+}
