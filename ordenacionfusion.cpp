c80710b2720fbb89b772420e73615ea547445d74
IvanPinna <ivanpinaarevalo@gmail.com>
2019-01-02 13:43:32 +0100

    fusion incompleto

--- ordenacionfusion.cpp
+++ ordenacionfusion.cpp
@@ -4,21 +4,70 @@
 
 using namespace std;
 
-/*int**/ ordenarporfusion(int* vector, int tam){
+int* insercion(int* vector, int tam){
+    int x, j;
+    
+    for(int i = 1; i < tam; ++i)
+    {
+        x = vector[i];
+        j = i - 1;
+        
+        while( j >= 0 && x < vector[j])
+        {
+            vector[j+1] = vector[j];
+            --j;
+        }
+        vector[j+1] = x;
+    }
+    
+    return vector;
+}
+
+int* fusionar(int* u, int* v, int tam1, int tam2)
+{
+    int j = 0;
+    int* res = new int[tam1+tam2];
+    
+    for(int i = 0; i < tam1; ++i)
+    {
+        res[j] = u[i];
+        ++j;
+    }
+    
+    for(int i = 0; i < tam2; ++i)
+    {
+        res[j] = v[i];
+        ++j;
+    }
+    
+    return res;
+}
+
+int* ordenarporfusion(int* vector, int tam){
     
     if(tam < 3)
     {
-        insercion(vector); //Ordenamos por insercion.
+        insercion(vector, tam); //Ordenamos por insercion.
     }
-    else
+    else //Dividir el array en dos subvectores.
     {
-        //Dividir el array en dos subvectores. 
+        int * u = ordenarporfusion(); 
+        int * v = ordenarporfusion();
+        fusionar(u , v, tam1, tam2);
     }
-    //return ;
+    
+    return vector;
 }
+
 
 int main()
 {
+    int tam = 13;
     int p[13] = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9};
-    fusion(p, 13); 
+    int* o = ordenarfusion(p, tam);
+    
+    for(int i = 0; i < tam; ++i)
+        cout << o[i] << endl;
+    
+    
 }
